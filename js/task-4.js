8f0499d9eaeb6e350b87840e5f89c44d19cbe93c
Mykola Kuchmynda <mykola.kuchmynda@gmail.com>
2025-02-16 12:17:55 +0200

    task-4 done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,20 @@
+document.querySelector(".login-form").addEventListener("submit", function (event) {
+  event.preventDefault();
 
+  const { email, password } = event.target.elements;
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+  if (!emailValue || !passwordValue) {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  const formData = {
+    email: emailValue,
+    password: passwordValue,
+  };
+
+  console.log(formData);
+  event.target.reset();
+});
